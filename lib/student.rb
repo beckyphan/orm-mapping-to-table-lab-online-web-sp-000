229a4316573ba09efccce06fc49f45e6bb8fed34
Becky Phan <beckyphan@live.com>
2019-07-23 20:41:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,12 @@
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade
+    @id = id 
+  end 
 
   def self.create_table
     sql =  <<-SQL 
